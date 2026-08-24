7d3c313635de897cefaf94ef03807fd5c01e8e00
Jamie Feingold <jfeingold@salesforce.com>
2026-08-24 13:26:58 -0500

    feat: rejiggered eslint.config.mjs (W-23928423)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,14 @@
-export { default } from 'eslint-config-salesforce-typescript';
+import tsconfigs from 'eslint-config-salesforce-typescript';
+
+const configs = [
+  ...tsconfigs,
+  {
+    rules: {
+      // Define convenient alias types
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/ban-types': 'off',
+    },
+  },
+];
+
+export default configs;
